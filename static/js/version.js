1840086e8feef49be06160670a3e5bc8ba3639f7
Abdulaziz-hu <aziz.f.alhuzami@gmail.com>
2026-05-09 21:54:19 +0300

    1.10.1: fixed broken links

--- static/js/version.js
+++ static/js/version.js
@@ -1,5 +1,5 @@
 // Change this variable to update the version everywhere
-const currentVersion = "1.10.0";
+const currentVersion = "1.10.1";
 
 // This finds every element with the ID and updates it automatically
 document.querySelectorAll('#app-version').forEach(el => {
